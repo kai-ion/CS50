9de518f20b5404e12197df710079d7ebce496357
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-06 03:39:19 +0000

    Mon, Jun 6, 2022, 3:39 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
 
+}
+
+int count_letters(string text) {
+
+}
+
+int count_words(string text) {
+
+}
+
+int count_sentences(string text) {
+    
 }

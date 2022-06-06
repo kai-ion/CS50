b3d492489ca0eeb90df2f939f547dceff2225090
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-06 21:00:21 +0000

    Mon, Jun 6, 2022, 9:00 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <set>
 
+using namespace std;
 
 bool valid_key(string str) {
-    
+    set<int> s;
+
     for (int i = 0; i < strlen(str); i++)
     {
         if (!isalpha(str[i]))

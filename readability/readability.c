8dda1e819461b76cf860b7d850957b6f946796b2
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-06 03:52:12 +0000

    Mon, Jun 6, 2022, 3:52 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string text)
+{
+    int sum = 0;
+
+    return sum;
+}
+
+int count_words(string text)
+{
+    int sum = 0;
+
+    return sum;
+}
+
+int count_sentences(string text)
+{
+    int sum = 0;
+
+    return sum;
+}
+
 int main(void)
 {
     string text = get_string("Enter text\n");
@@ -8,33 +29,16 @@ int main(void)
     int letter;
     int word;
     int sentence;
-    
+
     letter = count_letters(text);
     word = count_words(text);
     sentence = count_sentences(text);
 
-    int l = letter / word * 100;
-    int s = sentece / word * 100;
+    int L = letter / word * 100;
+    int S = sentence / word * 100;
 
     int index = 0.0588 * L - 0.296 * S - 15.8;
 
     printf("Grade: %d/n", index);
 }
 
-int count_letters(string text) {
-    int sum = 0
-
-    return sum;
-}
-
-int count_words(string text) {
-    int sum = 0
-
-    return sum;
-}
-
-int count_sentences(string text) {
-    int sum = 0
-
-    return sum;
-}

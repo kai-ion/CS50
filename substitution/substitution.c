b30204857ccc0ce0dc6c757065376d1a5dc6cf37
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-06 20:05:38 +0000

    Mon, Jun 6, 2022, 8:05 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,14 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
 bool valid_key(string str) {
-    
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (!isalpha(str[i]))
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main(int argc, string argv[])
 {
+    bool isValid = valid_key(argv[0]);
+
     if (strlen(argv[0]) != 26)
     {
         printf("Key must contain 26 characters.\n");

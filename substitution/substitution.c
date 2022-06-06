4990e68dcb71d1ccde34368b4c4527414b11206c
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-06 19:56:23 +0000

    Mon, Jun 6, 2022, 7:56 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,9 +4,13 @@
 
 int main(int argc, string argv[])
 {
-    if (strlen(argv) != 26)
+    if (strlen(argv[0]) != 26)
     {
         printf("Key must contain 26 characters.\n");
         return 1;
+    }
+    else if (argc != 1)
+    {
+        printf("Usage: ./substitution key\n");
     }
 }

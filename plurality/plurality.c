6ec5f000095c652af12a173e67b0b0ce22021555
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-07 20:51:16 +0000

    Tue, Jun 7, 2022, 8:51 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -69,7 +69,8 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for (int i = 0; i < candidate_count; i++) {
+    for (int i = 0; i < candidate_count; i++)
+    {
         if (strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes += 1;
@@ -96,7 +97,8 @@ void print_winner(void)
         }
     }
 
-    for (int i = 0; i < candidate_count; i++) {
+    for (int i = 0; i < candidate_count; i++)
+    {
         if (candidates[i].votes == max)
         {
             printf("%s\n", candidates[i].name);

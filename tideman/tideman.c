258975c6e3e9608b7e5a14a69016f224262bbf21
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-11 16:24:46 +0000

    Sat, Jun 11, 2022, 4:24 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -102,7 +102,7 @@ bool vote(int rank, string name, int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if ()
+        if (strcmp(candidates[i], name))
     }
     return false;
 }

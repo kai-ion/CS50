6ff6e2869d9373627ef8cb68abebf13740411208
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-12 22:00:43 +0000

    Sun, Jun 12, 2022, 10:00 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -165,14 +165,13 @@ void add_pairs(void)
 void sort_pairs(void)
 {
     // TODO
-    int i, j, x, y;
-    for (i = 0; i < pair_count - 1; i++)
+    for (int i = 0; i < pair_count - 1; i++)
 
         // Last i elements are already in place
-        for (j = 0; j <  pair_count - i - 1; j++)
+        for (int j = 0; j <  pair_count - i - 1; j++)
         {
-            x = preferences[pairs[j].winner][pairs[j].loser] - preferences[pairs[j].loser][pairs[j].winner];
-            y = preferences[pairs[j + 1].winner][pairs[j + 1].loser] - preferences[pairs[j + 1].loser][pairs[j + 1].winner];
+            int x = preferences[pairs[j].winner][pairs[j].loser] - preferences[pairs[j].loser][pairs[j].winner];
+            int y = preferences[pairs[j + 1].winner][pairs[j + 1].loser] - preferences[pairs[j + 1].loser][pairs[j + 1].winner];
             if (x > y)
             {
                 pair temp = pairs[j];

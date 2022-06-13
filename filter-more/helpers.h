c8aca6a15dbf79523443bb67c4c95014379e706c
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-13 22:43:15 +0000

    Mon, Jun 13, 2022, 10:43 PM +00:00

--- filter-more/helpers.h
+++ filter-more/helpers.h
@@ -4,7 +4,12 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 
 // Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width]);
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < width / 2; i++) {
+        
+    }
+};
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width]);

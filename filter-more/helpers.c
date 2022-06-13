e4cdf11d8598dad0420b2c050a9aaba9c9dd1aca
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-13 22:48:37 +0000

    Mon, Jun 13, 2022, 10:48 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -9,7 +9,14 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i <  height; i++) {
+        for (int j = 0; j < width/2; j++)
+        {
+            RGBTRIPLE temp = image[height][j];
+            image[height][j] = image[height][width - 1 - j];
+            image[height][width - 1 - j] = image[height][j];
+        }
+    }
     return;
 }
 
--- filter-more/helpers.h
+++ filter-more/helpers.h
@@ -4,15 +4,7 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 
 // Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width])
-{
-    for (int i = 0; i <  height; i++) {
-        for (int j = 0; j < width/2; j++)
-        {
-            RGBTRIPLE temp = image[height][j];
-        }
-    }
-};
+void reflect(int height, int width, RGBTRIPLE image[height][width]);
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width]);

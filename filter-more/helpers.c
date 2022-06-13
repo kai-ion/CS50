4a22fa51c64806feed3c6ad81ff21335608a9edf
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-13 23:43:52 +0000

    Mon, Jun 13, 2022, 11:43 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,6 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i <  height; i++) {
+        for (int j = 0; j < width/2; j++)
+        {
+            RGBTRIPLE temp = image[height][j];
+            image[height][j] = image[height][width - 1 - j];
+            image[height][width - 1 - j] = temp;
+        }
+    }
     return;
 }
 

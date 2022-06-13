8ea6010e14556828e129c775319ec4717b38b486
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-13 23:56:59 +0000

    Mon, Jun 13, 2022, 11:56 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -20,9 +20,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i <  height; i++) {
         for (int j = 0; j < width/2; j++)
         {
-            RGBTRIPLE temp = image[height][j];
-            image[height][j] = image[height][width - 1 - j];
-            image[height][width - 1 - j] = temp;
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
         }
     }
     return;

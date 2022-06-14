5ce959321a35947a14a6678baa8a9f82eb38d9be
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-14 12:19:43 +0000

    Tue, Jun 14, 2022, 12:19 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -32,7 +32,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    B
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)

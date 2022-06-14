1fd80d0cc50833c5b018296007500611afc9a2bd
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-14 12:20:51 +0000

    Tue, Jun 14, 2022, 12:20 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -5,14 +5,16 @@ int main(void)
 {
     int num = get_int("Enter a height between 1 - 8\n");
     //printf("Height: %d\n", num);
-    while ( num < 1 || num > 8) {
+    while ( num < 1 || num > 8)
+    {
         printf("Invalid Input\n");
         num = get_int("Enter a height between 1 - 8\n");
         //printf("Height: %d\n", num);
     }
 
 
-    for (int i = 0; i < num; i++) {
+    for (int i = 0; i < num; i++)
+    {
 
         for (int k = num - i - 1; k > 0; k--) {
             printf(" ");

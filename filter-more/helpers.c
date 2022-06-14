02bfdd32cdbf7473a186c2c864bc8741c628311c
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-14 01:26:30 +0000

    Tue, Jun 14, 2022, 1:26 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -36,6 +36,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+
             image[i][j].rgbtRed = image[i-1][j-1].rgbtRed
         }
     }

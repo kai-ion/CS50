b1787599a4914548609b686115e681093a6d4de9
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-14 01:23:39 +0000

    Tue, Jun 14, 2022, 1:23 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -36,7 +36,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            
+            image[i][j] = 
         }
     }
     return;

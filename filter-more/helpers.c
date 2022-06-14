79fd2fe43e120ca2f8dd054e6072c2c9b1960558
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-06-14 01:03:49 +0000

    Tue, Jun 14, 2022, 1:03 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -4,7 +4,7 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i <  height; i++) {
-        for (int j = 0; j < width/2; j++)
+        for (int j = 0; j < width; j++)
         {
             RGBTRIPLE temp = image[height][j];
             image[height][j] = image[height][width - 1 - j];

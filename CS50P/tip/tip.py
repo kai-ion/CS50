a8447dba0c04c7980a78bd5374cc316535e7988a
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-02 19:16:33 +0000

    Tue, Aug 2, 2022, 7:16 PM +00:00

--- CS50P/tip/tip.py
+++ CS50P/tip/tip.py
@@ -17,6 +17,7 @@ def dollars_to_float(d):
 def percent_to_float(p):
     # TODO
     #percent_to_float, which should accept a str as input (formatted as ##%, wherein each # is a decimal digit), remove the trailing %, and return the percentage as a float. For instance, given 15% as input, it should return 0.15.
-    p = p.replace()
+    p = float(p.replace('%',''))/100
+    return p
 
 main()

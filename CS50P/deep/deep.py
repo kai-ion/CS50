3108eec777b65b0a0e502f5873bcb6e27eceb02b
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-02 21:46:47 +0000

    Tue, Aug 2, 2022, 9:46 PM +00:00

--- CS50P/deep/deep.py
+++ CS50P/deep/deep.py
@@ -1,5 +1,5 @@
 def main() :
-    str = input("What is the answer to the Great Question of Life, the Universe and Everything ").lower().replace(' ', '')
+    str = input("What is the answer to the Great Question of Life, the Universe and Everything ").lower().strip()
 
     if str == "42" or str == "forty-two" or str == "forty two":
         print("yes")

66cb88be1a9ad9e5759920210fd168836b479e07
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-03 14:36:53 +0000

    Wed, Aug 3, 2022, 2:36 PM +00:00

--- CS50P/extensions/extensions.py
+++ CS50P/extensions/extensions.py
@@ -1,6 +1,21 @@
 def main() :
     str = input("File name: ")
 
-    if str
+    if ".gif" in str :
+        print("image/gif")
+    elif ".jpg" in str :
+        print("image/jpeg")
+    elif ".jpeg" in str :
+        print("image/jpeg")
+    elif ".png" in str :
+        print("image/png")
+    elif ".pdf" in str :
+        print("image/pdf")
+    elif ".txt" in str :
+        print("image/txt")
+    elif ".zip" in str :
+        print("image/zip")
+    else :
+        print("application/octet-stream")
 
 main()

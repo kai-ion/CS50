c4c6c61f3ff4bc7a6004814b5973c2a7cea03de3
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-15 23:55:09 +0000

    ddd, Aug 8/15/2022, YYYY, 11:55 A +0

--- CS50P/fuel/fuel.py
+++ CS50P/fuel/fuel.py
@@ -1,7 +1,14 @@
 def main() :
     num = int(input("Fraction: "))
-    
+
     ...
 
+def get_int(prompt)
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+        
 if __name__ == "__main__" :
     main()

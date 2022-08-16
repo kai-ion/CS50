41657586e3e84799681d3a2d6dbb72c3281d533f
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-16 17:37:57 +0000

    Tue, Aug 16, 2022, 5:37 PM +00:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -24,7 +24,6 @@ def get_int(prompt) :
             year, month, day = date.split("/")
             if int(month) >= 1 and int(month) <= 12 and int(day) >= 1 and int(day) <= 31:
                 break
-
         except:
             try:
                 month2, day2, year = date.split(" ")

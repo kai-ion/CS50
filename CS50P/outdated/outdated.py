827e25d02cd52409182a85863dff860fb6c4bfed
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-16 17:59:40 +0000

    Tue, Aug 16, 2022, 5:59 PM +00:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -28,6 +28,9 @@ def main() :
                     if month2 == months[i] :
                         month = i + 1
 
+                if not day2.endswith(","):
+                    continue
+
                 day = day2.replace("," , "")
 
                 if int(month) >= 1 and int(month) <= 12 and int(day) >= 1 and int(day) <= 31:

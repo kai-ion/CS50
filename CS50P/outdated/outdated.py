f5403b1a85629a9f6aacb0cc50ee040712daf8fa
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-16 17:14:34 +0000

    Tue, Aug 16, 2022, 5:14 PM +00:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -17,6 +17,21 @@ month = [
 def main() :
     get_int("")
 
+def get_int(prompt) :
+    while True:
+        try:
+            item = input(prompt).lower()
+            if item in grocery :
+                grocery[item] = grocery[item] + 1
+            else :
+                grocery[item] = 1
+
+        except (EOFError):
+            print()
+
+            for items in sorted(grocery.keys()) :
+                print(grocery[items], items.upper())
+            break
 
 if __name__ == "__main__" :
     main()

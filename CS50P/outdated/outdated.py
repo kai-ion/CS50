d9e5331f9f2c113833168f9b01e3820fb3e9c3b2
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-16 17:26:18 +0000

    Tue, Aug 16, 2022, 5:26 PM +00:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -22,7 +22,8 @@ def get_int(prompt) :
         date = input(prompt).lower()
         try:
             year, months, day = date.split("/")
-            print(f"{year}-{months}-{day}")
+            if int(months) >= 1 and int(months) <= 12 and int(day) >= 1 and int(day) <= 31:
+                print(f"{year}-{months}-{day}")
 
         except:
             print()

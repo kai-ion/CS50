7542505621006386a13ec38eeb23018cb9d615f7
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-16 17:13:47 +0000

    Tue, Aug 16, 2022, 5:13 PM +00:00

---
+++ CS50P/outdated/outdated.py
@@ -0,0 +1,23 @@
+month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
+def main() :
+    get_int("")
+
+
+if __name__ == "__main__" :
+    main()
+    ...

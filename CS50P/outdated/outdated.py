9f16e372ac83130680e8689fe25c9442c89584fc
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-16 17:53:57 +0000

    Tue, Aug 16, 2022, 5:53 PM +00:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -15,7 +15,7 @@ months = [
 
 def main() :
     while True:
-        date = input("")
+        date = input("Date: ")
         try:
             month, day, year = date.split("/")
             if int(month) >= 1 and int(month) <= 12 and int(day) >= 1 and int(day) <= 31:

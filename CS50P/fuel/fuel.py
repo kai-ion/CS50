83cba0355d09425493d2f6826892d9f32b5e3e44
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-16 00:03:56 +0000

    ddd, Aug 8/16/2022, YYYY, 12:03 A +0

--- CS50P/fuel/fuel.py
+++ CS50P/fuel/fuel.py
@@ -1,12 +1,25 @@
 def main() :
     num = get_int("Fraction: ")
+    newNum = num * 100
 
+    if newNum <= 1 :
+        print("E")
+    elif newNum >= 99:
+        print("F")
+    else :
+        print(f"{newNum}%")
     ...
 
 def get_int(prompt) :
     while True:
         try:
-            return int(input(prompt))
+            fuel = input(prompt)
+            numerator, denominator = fuel.split("/")
+            newNum = int(numerator)
+            newDen = int (denominator)
+            f = newNum / newDen
+            if f <= 1:
+                return f
         except (ValueError, ZeroDivisionError):
             pass
 

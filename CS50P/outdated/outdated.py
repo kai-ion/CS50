0d4eb2e44393c846f4be9eabfb6c6f197013c57e
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-16 17:33:46 +0000

    Tue, Aug 16, 2022, 5:33 PM +00:00

--- CS50P/outdated/outdated.py
+++ CS50P/outdated/outdated.py
@@ -35,7 +35,7 @@ def get_int(prompt) :
 
                 day = day.replace("," , "")
                 if int(month) >= 1 and int(month) <= 12 and int(day) >= 1 and int(day) <= 31:
-                    print(f"{year}-{months}-{day}")
+                    print(f"{year}-{int(month):02}-{int(day):02}")
             except:
                 print()
                 pass

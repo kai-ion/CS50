2b13d981bd61af5480a6a2cb385dbdb6606f406f
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-23 03:43:35 +0000

    Tue, Aug 23, 2022, 3:43 AM +00:00

--- CS50P/figlet/figlet.py
+++ CS50P/figlet/figlet.py
@@ -2,7 +2,11 @@ from pyfiglet import Figlet
 import sys
 
 def main() :
-    if len(sys.argv) == 3 and ar
+    if len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font") :
+        try :
+            ...
+        except :
+            ...
     ...
 
 if __name__ == "__main__" :

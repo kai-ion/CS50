6e777db8be8ab959279cbb1c7213f3b209463956
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-24 23:31:59 +0000

    Wed, Aug 24, 2022, 11:31 PM +00:00

--- CS50P/bitcoin/bitcoin.py
+++ CS50P/bitcoin/bitcoin.py
@@ -5,6 +5,10 @@ def main() :
 
     if sys.argv < 2 :
         sys.exit("Missing command-line argument")
+    try:
+        ...
+    except requests.RequestException:
+        ...
     ...
 
 if __name__ == "__main__" :

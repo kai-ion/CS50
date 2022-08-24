93580162eda603c07537219fe830254c3030aeac
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-24 14:43:50 +0000

    Wed, Aug 24, 2022, 2:43 PM +00:00

--- CS50P/game/game.py
+++ CS50P/game/game.py
@@ -1,5 +1,21 @@
+import random
 def main() :
-    
+    while True:
+        try :
+            num = int(input("Level: "))
+
+            if (num < 1):
+                continue
+
+            randNum = random.randint(1, num)
+            guess = int(input("Guess: "))
+
+            if guess < 1 :
+                continue
+            
+        except ValueError:
+            pass
+
     ...
 
 if __name__ == "__main__" :

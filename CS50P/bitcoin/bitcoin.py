ff065adabb9de685fa5c44d0a8f9fe2fd9112127
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-27 03:17:58 +0000

    Sat, Aug 27, 2022, 3:17 AM +00:00

--- CS50P/bitcoin/bitcoin.py
+++ CS50P/bitcoin/bitcoin.py
@@ -9,11 +9,11 @@ def main() :
 
 
     try:
-        n = float(sys.argv[2])
+        n = float(sys.argv[1])
         response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
         print(json.dumps(response.json(), indent=2))
         ...
-    except (requests.RequestException or ValueError):
+    except requests.RequestException or ValueError:
         if ValueError :
             sys.exit("Command-line argument is not a number")
         else :

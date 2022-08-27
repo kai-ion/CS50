9d79c0130981c986b96c695a13336cb9b8e8da93
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-27 03:14:39 +0000

    Sat, Aug 27, 2022, 3:14 AM +00:00

--- CS50P/bitcoin/bitcoin.py
+++ CS50P/bitcoin/bitcoin.py
@@ -1,14 +1,17 @@
 import sys
 import requests
+import json
 
 def main() :
 
-    if sys.argv < 2 :
+    if len(sys.argv) < 2 :
         sys.exit("Missing command-line argument")
 
 
     try:
         n = float(sys.argv[2])
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        print(json.dumps(response.json(), indent=2))
         ...
     except (requests.RequestException or ValueError):
         if ValueError :

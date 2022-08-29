29adf248c1492ed8f7bdd26a0508f3b39db6bc5c
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-29 02:19:07 +0000

    Mon, Aug 29, 2022, 2:19 AM +00:00

--- CS50P/test_twttr/test_twttr.py
+++ CS50P/test_twttr/test_twttr.py
@@ -1,12 +1,19 @@
 import twttr
 
 def main() :
+    test_cases()
     ...
 
 def test_cases() :
     assert twttr.shorten("twitter") == "twttr"
     assert twttr.shorten("TWITTER") == "TWTTR"
     assert twttr.shorten("TwItTeR") == "TwtTR"
+
+def test_number() :
+    assert twttr.shorten("1234") == "1234"
+
+def test_punctuation() :
+    assert twttr.shorten("?!.,") == "?!.,"
 
 if __name__ == "__main__" :
     main()

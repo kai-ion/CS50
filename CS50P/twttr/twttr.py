2c15fb87b96151331a70bb4dd2e31dfb7a240b37
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-08-29 02:00:23 +0000

    Mon, Aug 29, 2022, 2:00 AM +00:00

--- CS50P/twttr/twttr.py
+++ CS50P/twttr/twttr.py
@@ -14,6 +14,16 @@ main()
 '''
 
 def main() :
+    str = input("Input: ")
+
+    for i in str :
+
+        if i.upper() in ['A', 'E', 'I', 'O', 'U'] :
+            print('', end="")
+
+        else :
+            print(i, end="")
+    print()
     ...
 
 

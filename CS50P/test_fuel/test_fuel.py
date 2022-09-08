317d21465c9f5e96886c3aee3cd9607125d70f48
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-08 19:44:44 +0000

    Thu, Sep 8, 2022, 7:44 PM +00:00

--- CS50P/test_fuel/test_fuel.py
+++ CS50P/test_fuel/test_fuel.py
@@ -2,7 +2,13 @@ from fuel import convert,gauge
 
 
 def main () :
+    test_input()
     ...
+
+def test_input() :
+    assert convert("1/4") == 25 and gauge(25) == "25%"
+    assert convert("1/100") == 1 and gauge(25) == "E"
+    assert convert("99/100") == 1 and gauge(25) == "F"
 
 if __name__ == "__main__" :
     main()

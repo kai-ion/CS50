83e348a9c1fdb22d00a5c799edcb39185f1c3ddc
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-08 17:40:13 +0000

    Thu, Sep 8, 2022, 5:40 PM +00:00

--- CS50P/test_bank/bank.py
+++ CS50P/test_bank/bank.py
@@ -1,7 +1,7 @@
 def main() :
-    str = input("Greeting: ")
+    s = input("Greeting: ")
 
-    greeting = value(str)
+    greeting = value(s)
 
     print(f"${greeting}")
 
@@ -10,11 +10,11 @@ def value(greeting) :
     greeting = greeting.strip().lower()
 
     if "hello" in greeting :
-        return int(0)
+        return 0
     elif greeting[0] == 'h' :
-        return int(20)
+        return 20
     else :
-        return int(100)
+        return 100
 
 if __name__ == "__main__" :
     main()

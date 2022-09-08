308032dce3e1a7bb602802840d3816e1ef0fca2a
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-08 19:46:29 +0000

    Thu, Sep 8, 2022, 7:46 PM +00:00

--- CS50P/test_fuel/test_fuel.py
+++ CS50P/test_fuel/test_fuel.py
@@ -1,14 +1,19 @@
 from fuel import convert,gauge
-
+import pytest
 
 def main () :
     test_input()
+    test_zero_division()
     ...
 
 def test_input() :
     assert convert("1/4") == 25 and gauge(25) == "25%"
     assert convert("1/100") == 1 and gauge(25) == "E"
     assert convert("99/100") == 1 and gauge(25) == "F"
+
+def test_zero_division() :
+    with pytest.raises(ZeroDivisionError) :
+        convert("1/0")
 
 if __name__ == "__main__" :
     main()

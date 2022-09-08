68ed362bf424c2abbec74702f01b217871e5fe77
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-08 19:21:57 +0000

    Thu, Sep 8, 2022, 7:21 PM +00:00

--- CS50P/test_plates/test_plates.py
+++ CS50P/test_plates/test_plates.py
@@ -1,23 +1,37 @@
 from plates import is_valid
 
 def main() :
-    test_return()
-    is_valid_length()
-    is_valid_start()
 
-def test_return() :
-    assert is_valid("AA") == True
+    test_length()
+    test_start()
+    test_number()
+    test_0()
+    test_punctuation()
 
-def is_valid_length() :
+
+def test_length() :
     assert is_valid("AA") == True
     assert is_valid("AAAAAA") == True
     assert is_valid("A") == False
     assert is_valid("AAAAAAA") == False
 
-def is_valid_start() :
+def test_start() :
     assert is_valid("AA") == True
     assert is_valid("1A") == False
     assert is_valid("A1") == False
+
+def test_number() :
+    assert is_valid("AAA222") == True
+    assert is_valid("AAA22A") == False
+
+def test_0() :
+    assert is_valid("CS50") == True
+    assert is_valid("CS05") == False
+
+def test_punctuation() :
+    assert is_valid("CS.50") == False
+    assert is_valid("CS!05") == False
+    assert is_valid("CS 05") == False
 
 if __name__ == "__main__" :
     main()

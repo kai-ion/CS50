97be7137a83a01113b1c9adea362d7cae4e737b3
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-08 17:12:19 +0000

    Thu, Sep 8, 2022, 5:12 PM +00:00

--- CS50P/bank/bank.py
+++ CS50P/bank/bank.py
@@ -6,7 +6,7 @@ def main() :
     print(f"${output}")
 
 def value(greeting) :
-    if "hello" in str :
+    if "hello" in greeting :
         return 0
     elif greeting[0] == 'h' :
         return 20

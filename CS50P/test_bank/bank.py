30d03be2dd0dc6373efa0250664b141ba3e155e6
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-08 17:25:10 +0000

    Thu, Sep 8, 2022, 5:25 PM +00:00

---
+++ CS50P/test_bank/bank.py
@@ -0,0 +1,16 @@
+def main() :
+    str = input("Greeting: ").strip().lower()
+
+    output = value(str)
+
+    print(f"${output}")
+
+def value(greeting) :
+    if "hello" in greeting :
+        return 0
+    elif greeting[0] == 'h' :
+        return 20
+    else :
+        return 100
+
+main()

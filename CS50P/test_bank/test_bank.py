584e58c14a6e20a28330ac76463afe57ce57b453
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-08 17:16:20 +0000

    Thu, Sep 8, 2022, 5:16 PM +00:00

--- CS50P/test_bank/test_bank.py
+++ CS50P/test_bank/test_bank.py
@@ -1,6 +1,19 @@
 from bank import value
 
 def main() :
+    test_return_0()
+    ...
+
+def test_return_0():
+    assert value("hello") == 0
+    assert value("Hello") == 0
+    assert value("Hello, world") == 0
+    ...
+
+def test_return_20():
+    ...
+
+def test_return_100():
     ...
 
 if __name__ == "__main__" :

e81cdcc29faa4fcbaca7296aa4ca5f4ffdf5dacc
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-08 17:30:37 +0000

    Thu, Sep 8, 2022, 5:30 PM +00:00

--- CS50P/test_bank/test_bank.py
+++ CS50P/test_bank/test_bank.py
@@ -2,15 +2,19 @@ import bank
 
 def main() :
     test_return_0()
+    test_return_20()
+    test_return_100()
     ...
 
 def test_return_0():
     assert bank.value('hello') == 0
-    #assert bank.value("Hello") == 0
-    #assert bank.value("Hello, world") == 0
+    assert bank.value("Hello") == 0
+    assert bank.value("Hello, world") == 0
     ...
 
 def test_return_20():
+    assert bank.value('hi') == 20
+    assert bank.value("Hey") == 20
     ...
 
 def test_return_100():

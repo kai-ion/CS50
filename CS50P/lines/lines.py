a95034bbd1ade182dadbd5a14c16df0c0c2a99fb
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-09 19:14:43 +0000

    Fri, Sep 9, 2022, 7:14 PM +00:00

---
+++ CS50P/lines/lines.py
@@ -0,0 +1,11 @@
+import sys
+
+def main() :
+    if len(sys.argv) < 2 :
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2 :
+        sys.exit("Too many command-line arguments")
+    ...
+
+if __name__ == "__main__" :
+    main()

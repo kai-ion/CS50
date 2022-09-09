f08a4ae353f65d5e7d6a4a0cc9e25192c57d054c
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-09 20:11:23 +0000

    Fri, Sep 9, 2022, 8:11 PM +00:00

---
+++ CS50P/pizza/pizza.py
@@ -0,0 +1,18 @@
+import sys
+import tabulate
+
+def main() :
+    check_command_line_arg()
+    ...
+
+def check_command_line_arg() :
+    if len(sys.argv) < 2 :
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2 :
+        sys.exit("Too many command-line arguments")
+
+    if ".py" not in sys.argv[1] :
+        sys.exit("Not a Python file")
+
+if __name__ == "__main__" :
+    main()

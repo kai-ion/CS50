b4ec1c329572e9574ec6cd54d056356850d5580b
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-10 14:55:14 +0000

    Sat, Sep 10, 2022, 2:55 PM +00:00

--- CS50P/scourgify/scourgify.py
+++ CS50P/scourgify/scourgify.py
@@ -1,5 +1,15 @@
 def main() :
     ...
 
+
+def check_command_line_arg() :
+    if len(sys.argv) < 2 :
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2 :
+        sys.exit("Too many command-line arguments")
+
+    if ".py" not in sys.argv[1] :
+        sys.exit("Not a Python file")
+
 if __name__ == "__main__" :
     main()

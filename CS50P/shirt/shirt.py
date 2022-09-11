c9e3d0921d7a9e46f7b2f3d575fc82ff8b1febbe
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-11 01:01:59 +0000

    Sun, Sep 11, 2022, 1:01 AM +00:00

--- CS50P/shirt/shirt.py
+++ CS50P/shirt/shirt.py
@@ -1,5 +1,16 @@
+import sys
+
 def main() :
     ...
+
+def check_command_line_arg() :
+    if len(sys.argv) < 3 :
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 3 :
+        sys.exit("Too many command-line arguments")
+
+    if ".csv" not in sys.argv[1] or ".csv" not in sys.argv[2]:
+        sys.exit("Not a CSV file")
 
 if __name__ == "__main__" :
     main()

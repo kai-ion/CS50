e4dfa87660206d20e6069af0c375b9d82ee253f6
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-11 01:14:11 +0000

    Sun, Sep 11, 2022, 1:14 AM +00:00

--- CS50P/shirt/shirt.py
+++ CS50P/shirt/shirt.py
@@ -13,12 +13,14 @@ def check_command_line_arg() :
     file1 = splitext(sys.argv[1])
     file2 = splitext(sys.argv[2])
 
-    if check_extension(file1) == False:
+    print(file1)
+    if check_extension(file1[1]) == False:
         sys.exit("Invalid input")
-    if check_extension(file2) == False:
+    if check_extension(file2[1]) == False:
         sys.exit("Invalid input")
 
-    if file1
+    if file1[1].lower() != file2[1].lower() :
+        sys.exit("Input and output have different extensions")
 
 def check_extension(file) :
     if file in [".jpg", ".jepg", ".png"]:

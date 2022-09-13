9bc55674cf0d2ca3f0d313409d3baaa4b6514c13
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-13 01:04:25 +0000

    Tue, Sep 13, 2022, 1:04 AM +00:00

--- CS50P/watch/watch.py
+++ CS50P/watch/watch.py
@@ -8,7 +8,7 @@ def main():
 
 def parse(s):
     if re.search(r"<iframe(.)*><\/iframe>", s) :
-        if match := re.search(r"^https?://(?:www\.)?youtube\.com/embed/", s) :
+        if match := re.search(r"^https?://(?:www\.)?youtube\.com/embed/([a-z0-9_]+)", s, re.IGNORECASE) :
             url = match.group(3)
 
             return "https://youtu.be/" + url

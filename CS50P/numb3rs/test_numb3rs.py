7192c56046459a21128bd330511adc38ecf1b0bf
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-13 00:17:28 +0000

    Tue, Sep 13, 2022, 12:17 AM +00:00

--- CS50P/numb3rs/test_numb3rs.py
+++ CS50P/numb3rs/test_numb3rs.py
@@ -6,12 +6,17 @@ def main() :
     ...
 
 def test_format() :
-    assert numb3rs.validate("1.2.3.4") == True
-    assert numb3rs.validate("1.2.3.") == False
+    assert numb3rs.validate(r"1.2.3.4") == True
+    assert numb3rs.validate(r"1.2.3.") == False
+    assert numb3rs.validate(r"cat") == False
+
 
     ...
 
 def test_range() :
+    assert numb3rs.validate(r"1.2.3.255") == True
+    assert numb3rs.validate(r"1.2.3.1000") == False
+
     ...
 
 if __name__ == "__main__" :

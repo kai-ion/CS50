9e8e1478db157e0e2ec851cab1f5c528abe172a7
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-14 03:07:22 +0000

    Wed, Sep 14, 2022, 3:07 AM +00:00

--- CS50P/working/working.py
+++ CS50P/working/working.py
@@ -8,8 +8,8 @@ def main():
 
 def convert(s):
     if match := re.search(r"^(([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M) to (([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M)$", s) :
-        if int(match.group(1)) > 12 or int(match.group(5)) > 12 :
-            raise ValueError
+ #       if int(match.group(1)) > 12 or int(match.group(5)) > 12 :
+   #         raise ValueError
         return match.groups()
     else :
         return ValueError

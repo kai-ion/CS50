6b0e4406e2d09b724eb609cc5e7c6434713ca613
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-14 22:40:37 +0000

    Wed, Sep 14, 2022, 10:40 PM +00:00

--- CS50P/working/test_working.py
+++ CS50P/working/test_working.py
@@ -18,7 +18,7 @@ def test_format() :
 def test_time() :
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert convert("10 PM to 8 AM") == "22:00 to 08:00"
-    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
+    assert convert("10:30 PM to 8c50 AM") == "22:30 to 08:50"
     ...
 
 def test_hour() :

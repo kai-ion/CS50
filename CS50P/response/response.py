1b2718e592f371b7b0527bd8d8040d2ad7c1fe98
kaiion <43284426+kai-ion@users.noreply.github.com>
2022-09-15 01:40:47 +0000

    Thu, Sep 15, 2022, 1:40 AM +00:00

--- CS50P/response/response.py
+++ CS50P/response/response.py
@@ -1,4 +1,10 @@
+from validator_collection import validators, checkers, errors
+
 def main() :
+    mail = validate(input("What's your email address?"))
+    ...
+
+def validate(s) :
     ...
 
 if __name__ == "__main__" :
